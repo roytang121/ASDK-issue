972252a9ceb01c781394600763843680b15f7ca9
Roy Tang <me@roytang.me>
2016-08-29 00:01:52 +0900

    Initial Commit

---
+++ ASDK Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ASDK Sample
+//
+//  Created by Roy Tang on 29/8/2016.
+//  Copyright © 2016 NA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
